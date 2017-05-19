647f8313e0f4d6c526d174a4f4fcf2e875477151
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-18 22:40:22 -0500

    #1. Added autoprefixer stylus to gulptasks.

--- aurelia_project/tasks/process-css.js
+++ aurelia_project/tasks/process-css.js
@@ -4,11 +4,12 @@ import sourcemaps from 'gulp-sourcemaps';
 import stylus from 'gulp-stylus';
 import project from '../aurelia.json';
 import {build} from 'aurelia-cli';
+import autoprefixerStylus from 'autoprefixer-stylus';
 
 export default function processCSS() {
   return gulp.src(project.cssProcessor.source)
     .pipe(changedInPlace({firstPass: true}))
     .pipe(sourcemaps.init())
-    .pipe(stylus())
+    .pipe(stylus({use: [autoprefixerStylus('last 2 versions')]}))
     .pipe(build.bundle());
 }
