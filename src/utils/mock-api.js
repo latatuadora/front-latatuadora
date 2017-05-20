51431b409498c0a63d3a8a74d140ddc701871a05
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-19 21:09:36 -0500

    #2. Added profileURL to mock API.

--- src/utils/mock-api.js
+++ src/utils/mock-api.js
@@ -6,56 +6,64 @@ let featuredArtists = [
     rating: 3.5,
     location: 'Roma, Ciudad de México',
     tags: ['Tag1', 'Tag2'],
-    imageURL: '/src/assets/images/mock/artist1.png'
+    imageURL: '/src/assets/images/mock/artist1.png',
+    profileURL: '#'
   },
   {
     name: 'Mock Artist 2',
     rating: 4.5,
     location: 'Roma, Ciudad de México',
     tags: ['Tag1', 'Tag2', 'Tag3'],
-    imageURL: '/src/assets/images/mock/artist2.png'
+    imageURL: '/src/assets/images/mock/artist2.png',
+    profileURL: '#'
   },
   {
     name: 'Mock Artist 3',
     rating: 5,
     location: 'Roma, Ciudad de México',
     tags: ['Tag1', 'Tag2'],
-    imageURL: '/src/assets/images/mock/artist3.png'
+    imageURL: '/src/assets/images/mock/artist3.png',
+    profileURL: '#'
   },
   {
     name: 'Mock Artist 4',
     rating: 4,
     location: 'Roma, Ciudad de México',
     tags: ['Tag1', 'Tag2', 'Tag3'],
-    imageURL: '/src/assets/images/mock/artist4.png'
+    imageURL: '/src/assets/images/mock/artist4.png',
+    profileURL: '#'
   },
   {
     name: 'Mock Artist 5',
     rating: 4.5,
     location: 'Roma, Ciudad de México',
     tags: ['Tag1', 'Tag2'],
-    imageURL: '/src/assets/images/mock/artist5.png'
+    imageURL: '/src/assets/images/mock/artist5.png',
+    profileURL: '#'
   },
   {
     name: 'Mock Artist 6',
     rating: 3.5,
     location: 'Roma, Ciudad de México',
     tags: ['Tag1', 'Tag2'],
-    imageURL: '/src/assets/images/mock/artist6.png'
+    imageURL: '/src/assets/images/mock/artist6.png',
+    profileURL: '#'
   },
   {
     name: 'Mock Artist 7',
     rating: 5,
     location: 'Roma, Ciudad de México',
     tags: ['Tag1', 'Tag2', 'Tag3'],
-    imageURL: '/src/assets/images/mock/artist7.png'
+    imageURL: '/src/assets/images/mock/artist7.png',
+    profileURL: '#'
   },
   {
     name: 'Mock Artist 8',
     rating: 3.9,
     location: 'Roma, Ciudad de México',
     tags: ['Tag1', 'Tag2'],
-    imageURL: '/src/assets/images/mock/artist8.png'
+    imageURL: '/src/assets/images/mock/artist8.png',
+    profileURL: '#'
   }
 ];
 
