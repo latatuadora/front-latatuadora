3d6bcc3f7e8f807db7e8fce33405666291d500c1
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-20 16:07:55 -0500

    #3. Migrated inspirate view-model from angular controller.

--- src/pages/inspirate/inspirate.js
+++ src/pages/inspirate/inspirate.js
@@ -1,3 +1,35 @@
+import {inject} from 'aurelia-framework';
+import {MockAPI} from 'utils/mock-api'
+
+@inject(MockAPI)
 export class Inspirate {
-  
+  constructor(api) {
+    this.api = api;
+    this.params = {
+      style: '',
+      element: '',
+      part: '',
+      page: 1
+    }
+    this.tattoos = [];
+    this.showLoader = false;
+    this.loadMore();
+  }
+
+  filterTattoos() {
+    this.page = 1;
+    this.tattoos = [];
+    this.loadMore();
+  }
+
+  loadMore() {
+    this.showLoader = true;
+    this.api.getTattoos(this.filters, this.page)
+      .then(tattoos => {
+        tattoos.forEach(tattoo => {
+          this.tattoos.push(tattoo);
+          this.showLoader = false;
+        });
+      });
+  }
 }
