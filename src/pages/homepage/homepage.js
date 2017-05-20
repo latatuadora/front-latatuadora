9b347d7ba9a04cda3a496c399b616883144fd4e1
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-19 20:26:58 -0500

    #1. Homepage now uses mock API.

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,6 +1,17 @@
+import {MockAPI} from 'utils/mock-api';
+import {inject} from 'aurelia-framework';
+
+@inject(MockAPI)
 export class Homepage {
-  constructor() {
+  constructor(api) {
+    this.api = api;
     this.iWantTo = 1;
+    this.featuredArtists = [];
+  }
+
+  created() {
+    this.api.getFeaturedArtists()
+      .then(artists => this.featuredArtists = artists);
   }
 
   changeIWantTo(iWantToOption) {
