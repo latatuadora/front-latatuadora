e995fd9756b306b8041d3ded2213ee09b5283669
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-20 14:47:31 -0500

    #3. Injected aurelia router into navbar.

--- src/resources/elements/unlogged-navbar/unlogged-navbar.js
+++ src/resources/elements/unlogged-navbar/unlogged-navbar.js
@@ -1,7 +1,18 @@
+import {Router} from 'aurelia-router';
+import {inject} from 'aurelia-framework';
 import {Menu} from 'utils/menu';
 
+@inject(Router)
 export class UnloggedNavbar {
+  constructor(router) {
+    this.router = router;
+  }
+
   attached() {
     let menu = new Menu(992);
+  }
+
+  getRoute() {
+    return this.router.currentInstruction.config.name;
   }
 }
