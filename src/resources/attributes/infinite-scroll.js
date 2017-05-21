63cd962688e0720952c47bbbb8c4815e19c67d5d
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-20 20:11:15 -0500

    #3. Created custom infinite scroll attribute.

---
+++ src/resources/attributes/infinite-scroll.js
@@ -0,0 +1,41 @@
+import {bindable, inject} from 'aurelia-framework';
+
+@inject(Element)
+export class InfiniteScrollCustomAttribute {
+  @bindable callback;
+
+  self = {};
+
+  constructor(element) {
+    this.element = element;
+  }
+
+  attached() {
+    self = this;
+    this.checkPosition();
+    document.addEventListener('scroll', this.checkPosition);
+    document.addEventListener('resize', this.changeTopPosition);
+  }
+
+  detached() {
+    document.removeEventListener('scroll', this.checkPosition);
+    document.removeEventListener('resize', this.changeTopPosition);
+  }
+
+  changeTopPosition() {
+    self.top = self.element.getBoundingClientRect().top;
+  }
+
+  checkPosition() {
+    let scroll = window.pageYOffset;
+    let clientRect = self.element.getBoundingClientRect();
+    self.top = self.top ? self.top : clientRect.top;
+
+    let triggerAt = clientRect.height - (self.top / 2) + 150;
+    let trigger = triggerAt < scroll;
+
+    if (trigger && clientRect.height) {
+      self.callback();
+    }
+  }
+}
