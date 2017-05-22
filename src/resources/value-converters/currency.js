8e59cb25b64673e486a7aa0392403fb604e77122
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-21 21:38:13 -0500

    #8. Fixed currency value converter.

--- src/resources/value-converters/currency.js
+++ src/resources/value-converters/currency.js
@@ -2,6 +2,7 @@ import numeral from 'numeral';
 
 export class CurrencyValueConverter {
   toView(value, currency) {
-    return numeral(value).format('$ 0,0.00') + currency;
+    let currencyText = currency ? ' ' + currency : '';
+    return numeral(value).format('$ 0,0.00') + ' ' + currencyText;
   }
 }
