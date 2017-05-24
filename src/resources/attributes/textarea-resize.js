2d93420b3e4fa4945815b86bb5249c6d081e153b
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-24 16:04:49 -0500

    #11. Created textarea-resize logic.

--- src/resources/attributes/textarea-resize.js
+++ src/resources/attributes/textarea-resize.js
@@ -1,8 +1,19 @@
 import {bindable, inject} from 'aurelia-framework';
 
 @inject(Element)
-export class TestareaResizeCustomAttribute {
+export class TextareaResizeCustomAttribute {
+  @bindable text;
+
   constructor(element) {
     this.element = element;
+  }
+
+  textChanged() {
+    this.resize();
+  }
+
+  resize() {
+    this.element.style.height = this.element.style.minHeight;
+    this.element.style.height = this.element.scrollHeight + 'px';
   }
 }
