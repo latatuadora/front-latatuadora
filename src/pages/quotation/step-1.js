9e7c87b03ea62884257b83ca98487b2d5ca36ebf
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-25 23:23:24 -0500

    #9. Implemented shared object and range-slider in quotation step 1.

--- src/pages/quotation/step-1.js
+++ src/pages/quotation/step-1.js
@@ -1,6 +1,7 @@
 export class Step1 {
-  activate(artist) {
-    this.general = artist == null ? true : false;
-    this.artist = artist;
+  activate(model) {
+    this.model = model;
+    this.general = model.artist == null ? true : false;
+    this.artist = model.artist;
   }
 }
