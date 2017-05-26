c54e4daebca9da58979bc424a0df08e32ca3b902
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-25 19:45:57 -0500

    #9. Copied multi-step base logic from github repository: https://github.com/ScottWhittaker/aurelia-multi-step-view-demo/blob/master/src/components/multi-step-view.js

---
+++ src/utils/base-multi-step.js
@@ -0,0 +1,59 @@
+export class MultiStepViewCustomElement {
+  backButtonActive = false;
+  completeButtonActive = false;
+  nextButtonActive = true;
+
+  currentViewModelPath;
+  currentStep = 1;
+  totalSteps = 0;
+
+  composeRef;
+  model = {};
+
+  bind() {
+    if (!this.cancel) {
+      throw new Error('bindable property `cancel` must be defined on MultiStepViewCustomElement');
+    }
+    if (!this.complete) {
+      throw new Error('bindable property `complete` must be defined on MultiStepViewCustomElement');
+    }
+    if (!this.viewModels) {
+      throw new Error('bindable property `viewModels` must be defined on MultiStepViewCustomElement');
+    }
+    this.totalSteps = this.viewModels.length;
+    this._update();
+  }
+
+  back() {
+    this.currentStep--;
+    this._update();
+  }
+
+  next() {
+    if (!this._isValidView()) return;
+    this.currentStep++;
+    this._update();
+  }
+
+  _update() {
+    this._setButtons();
+    this._setViewModel();
+  }
+
+  _setViewModel() {
+    this.currentViewModelPath = this.viewModels[this.currentStep - 1];
+  }
+
+  _setButtons() {
+    this.backButtonActive = this.currentStep > 1;
+    this.nextButtonActive = this.currentStep < this.totalSteps;
+    this.completeButtonActive = this.currentStep === this.totalSteps;
+  }
+
+  _isValidView() {
+    if (!this.composeRef.currentViewModel.isValid) {
+      throw new Error(`${this.composeRef.viewModel} must implement an isValid method returning a boolean`);
+    }
+    return this.composeRef.currentViewModel.isValid();
+  }
+}
