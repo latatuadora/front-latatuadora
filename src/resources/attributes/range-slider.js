38387a79e038d2152bfbbf1395b02f480b500ff3
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-25 23:22:14 -0500

    #9. Created slider logic.

--- src/resources/attributes/range-slider.js
+++ src/resources/attributes/range-slider.js
@@ -1,3 +1,38 @@
+import {inject, bindable} from 'aurelia-framework';
+import nouislider from 'nouislider';
+
+@inject(Element)
 export class RangeSliderCustomAttribute {
-  
+  @bindable update;
+  @bindable min;
+  @bindable max;
+  @bindable start;
+  @bindable unit;
+
+  constructor(element) {
+    this.element = element;
+  }
+
+  bind() {
+    nouislider.create(this.element, {
+      start: this.start,
+      range: {
+        min: this.min,
+        max: this.max
+      },
+      format: {
+        from: (value) => {return value.replace(` ${this.unit}`, '');},
+        to: (value) => {return value + ` ${this.unit}`;}
+      },
+      step: 1
+    });
+    this.element.noUiSlider.on('update', (values) => {
+      this.update(parseInt(values[0].replace(` ${this.unit}`, '')));
+    });
+  }
+
+  unbind() {
+    this.element.noUiSlider.off();
+    this.element.noUiSlider.destroy();
+  }
 }
