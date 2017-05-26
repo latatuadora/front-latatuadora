74bcf3b2d0d60e34a90bb5bac5f38cd6839eaba5
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-25 21:02:50 -0500

    #9. Base logic for complete function.

--- src/pages/quotation/quotation.js
+++ src/pages/quotation/quotation.js
@@ -1,11 +1,17 @@
+import {inject} from 'aurelia-framework';
+import {Router, Redirect} from 'aurelia-router';
 import {BaseMultiStep} from 'utils/base-multi-step';
 
+@inject(Router)
 export class Quotation extends BaseMultiStep {
-  constructor() {
+  constructor(router) {
     super();
+    this.router = router;
+    this.completeDestination = '';
     this.viewModels = [
       'pages/quotation/step-1'
     ];
+    this.resultsModel = {};
   }
 
   cancel() {
@@ -13,6 +19,12 @@ export class Quotation extends BaseMultiStep {
   }
 
   complete() {
-
+    for (let i = 0; i < this.router.routes.length; i++) {
+      if (this.router.routes[i].name == this.completeDestination) {
+        this.router.routes[i].settings.resultsModel = this.resultsModel;
+        break;
+      }
+    }
+    this.router.navigateToRoute(this.completeDestination);
   }
 }
