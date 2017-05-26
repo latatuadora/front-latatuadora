04ca028044438626e25fa503ada968e929ebf5a3
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-26 17:00:25 -0500

    #9. Base input file implementation.

--- src/pages/quotation/step-4/step-4.js
+++ src/pages/quotation/step-4/step-4.js
@@ -1,3 +1,20 @@
 export class Step4 {
+  constructor() {
+    this.maxFileSize = 5 * 1024;
+    this.fileValidations = [true, true];
+  }
 
+  activate(model) {
+    this.model = model;
+  }
+
+  onLoaded(file, data) {
+  }
+
+  onError(file, data) {
+  }
+
+  isValid() {
+    return true;
+  }
 }
