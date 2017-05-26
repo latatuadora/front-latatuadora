aaac70ee161f1d993639f8ce609d8219abadc169
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-25 20:10:34 -0500

    #9. Added step-1 to quotation viewModels array.

--- src/pages/quotation/quotation.js
+++ src/pages/quotation/quotation.js
@@ -1,9 +1,11 @@
-import {BaseMultiStep} from 'utils/base-multi-step'
+import {BaseMultiStep} from 'utils/base-multi-step';
 
 export class Quotation extends BaseMultiStep {
   constructor() {
     super();
-    this.viewModels = [];
+    this.viewModels = [
+      'pages/quotation/step-1'
+    ];
   }
 
   cancel() {
