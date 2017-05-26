6c91af34d273ecbd11e98b4ec4c24f8b043aaece
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-26 10:43:02 -0500

    #9. Step 2 now uses mock API.

--- src/pages/quotation/step-2.js
+++ src/pages/quotation/step-2.js
@@ -1,3 +1,30 @@
+import {inject} from 'aurelia-framework';
+import {MockAPI} from 'utils/mock-api';
+
+@inject(MockAPI)
 export class Step2 {
-  
+  constructor(api) {
+    this.api = api;
+    this.items = [];
+  }
+
+  activate(model) {
+    this.model = model;
+    this.getItems();
+  }
+
+  getItems() {
+    this.api.getStyles()
+      .then((styles) => {
+        if (this.model.style == null) {
+          styles[0].active = true;
+        }
+        styles.forEach((style) => {
+          if (this.model.style == style.style_name) {
+            style.active = true;
+          }
+          this.items.push(style);
+        });
+      });
+  }
 }
