67667473d930e07ee23a76b0466ae6f32eefa947
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-25 19:53:20 -0500

    #9. Base implementation of multi step inheritance in Quotation class.

--- src/pages/quotation/quotation.js
+++ src/pages/quotation/quotation.js
@@ -1,3 +1,16 @@
-export class Quotation {
-  
+import {BaseMultiStep} from 'utils/base-multi-step'
+
+export class Quotation extends BaseMultiStep {
+  constructor() {
+    super();
+    this.viewModels = [];
+  }
+
+  cancel() {
+
+  }
+
+  complete() {
+
+  }
 }
