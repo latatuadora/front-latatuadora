142234b38ef60369020b43f7f9e9ef00901bedb2
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-27 16:24:18 -0500

    #9, #20. Base step 3 logic.

--- src/pages/quotation/step-3/step-3.js
+++ src/pages/quotation/step-3/step-3.js
@@ -1,5 +1,27 @@
+import {inject} from 'aurelia-framework';
+import {MockAPI} from 'utils/mock-api';
+
+@inject(MockAPI)
 export class Step3 {
+  constructor(api) {
+    this.api = api;
+    this.bodyParts = {};
+  }
+
+  activate(model) {
+    this.model = model;
+    this.selectedId = model.bodyPart;
+    this.getBodyParts();
+  }
+
+  getBodyParts() {
+    this.api.getBodyParts()
+      .then(data => {
+        this.bodyParts = data;
+      });
+  }
+
   isValid() {
-    return true;
+    return this.model.bodyPart.type != '' && this.model.bodyPart.id != -1;
   }
 }
