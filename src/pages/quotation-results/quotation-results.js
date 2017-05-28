c503cce4b5432a2afa38db6f0a56e587b8892606
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-28 18:03:21 -0500

    #9, #23. Quotation results gets data from quotation.

--- src/pages/quotation-results/quotation-results.js
+++ src/pages/quotation-results/quotation-results.js
@@ -1,3 +1,22 @@
+import {inject} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+import {MockAPI} from 'utils/mock-api';
+
+@inject(Router, MockAPI)
 export class QuotationResults {
-  
+  constructor(router, api) {
+    this.router = router;
+    this.api = api;
+    this.results = {};
+  }
+
+  activate(params, routeConfig) {
+    if (routeConfig.settings.resultsModel) {
+      this.results = routeConfig.settings.resultsModel;
+      localStorage.setItem('quotationResults', JSON.stringify(this.results));
+    }
+    else {
+      this.results = JSON.parse(localStorage.getItem('quotationResults'));
+    }
+  }
 }
