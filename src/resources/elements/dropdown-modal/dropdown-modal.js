66e7220cdb8113c124c42e04f22e74c28ad939f8
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-27 22:15:22 -0500

    #9, #20. Minor hack for precise relocation of dropdown modal.

--- src/resources/elements/dropdown-modal/dropdown-modal.js
+++ src/resources/elements/dropdown-modal/dropdown-modal.js
@@ -8,6 +8,7 @@ export class DropdownModal {
   @bindable activeId;
   @bindable refElement;
   @bindable show;
+  @bindable showDropdown;
   @bindable close;
   @bindable topReference;
 
@@ -17,9 +18,18 @@ export class DropdownModal {
 
   attached() {
     let clientRect = this.refElement.getBoundingClientRect();
-    this.adjustCoordinates(clientRect, true, true);
     window.addEventListener('scroll', this.onScroll);
     window.addEventListener('resize', this.onResize);
+    this.adjustCoordinates(clientRect, true, true);
+  }
+
+  showDropdownChanged(newValue) {
+    if (newValue && this.elementsList.offsetWidth == 0) {
+      setTimeout(() => {
+        let clientRect = this.refElement.getBoundingClientRect();
+        this.adjustCoordinates(clientRect, true, true);
+      }, 10);
+    }
   }
 
   selectItem(item) {
@@ -38,19 +48,24 @@ export class DropdownModal {
   }
 
   adjustCoordinates(clientRect, adjustTop, adjustLeft) {
-    if (adjustTop) {
-      let topLimit = document.querySelector(this.topReference).offsetHeight;
-      if (clientRect.top + clientRect.height > topLimit) {
-        this.elementsList.style.top = clientRect.top + clientRect.height + 'px';
-      } else {
-        this.elementsList.style.top = topLimit + 'px';
+    if (this.showDropdown) {
+      if (adjustTop) {
+        let topLimit = document.querySelector(this.topReference).offsetHeight;
+        if (clientRect.top + clientRect.height > topLimit) {
+          this.elementsList.style.top = clientRect.top + clientRect.height + 'px';
+        } else {
+          this.elementsList.style.top = topLimit + 'px';
+        }
       }
-    }
-    if (adjustLeft) {
-      if (clientRect.left + this.elementsList.offsetWidth < window.innerWidth) {
-        this.elementsList.style.left = clientRect.left + 'px';
-      } else {
-        this.elementsList.style.left = '0px';
+      if (adjustLeft) {
+        if (clientRect.left + this.elementsList.offsetWidth < window.innerWidth) {
+          this.elementsList.style.left = clientRect.left + 'px';
+        } else if (this.elementsList.offsetWidth < window.innerWidth){
+          let difference = window.innerWidth - this.elementsList.offsetWidth;
+          this.elementsList.style.left = (difference / 2) + 'px';
+        } else {
+          this.elementsList.style.left = '0px';
+        }
       }
     }
   }
