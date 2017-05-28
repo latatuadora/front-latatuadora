579a0228e013eccc1e9f2f48e375ee2eb36c8484
Alan Sanchez <sanchezpineda03@gmail.com>
2017-05-27 21:08:12 -0500

    #9, #20. Dropdown modal logic, styles and final structure.

--- src/resources/elements/dropdown-modal/dropdown-modal.js
+++ src/resources/elements/dropdown-modal/dropdown-modal.js
@@ -1,8 +1,57 @@
-import {bindable} from 'aurelia-framework';
+import {bindable, inject} from 'aurelia-framework';
 
+@inject(Element)
 export class DropdownModal {
   @bindable list;
   @bindable onClick;
   @bindable itemName;
   @bindable activeId;
+  @bindable refElement;
+  @bindable show;
+  @bindable close;
+  @bindable topReference;
+
+  constructor(element) {
+    this.element = element;
+  }
+
+  attached() {
+    let clientRect = this.refElement.getBoundingClientRect();
+    this.adjustCoordinates(clientRect, true, true);
+    window.addEventListener('scroll', this.onScroll);
+    window.addEventListener('resize', this.onResize);
+  }
+
+  selectItem(item) {
+    this.onClick(item);
+    this.close();
+  }
+
+  onResize = () => {
+    let clientRect = this.refElement.getBoundingClientRect();
+    this.adjustCoordinates(clientRect, true, true);
+  }
+
+  onScroll = () => {
+    let clientRect = this.refElement.getBoundingClientRect();
+    this.adjustCoordinates(clientRect, true, false);
+  }
+
+  adjustCoordinates(clientRect, adjustTop, adjustLeft) {
+    if (adjustTop) {
+      let topLimit = document.querySelector(this.topReference).offsetHeight;
+      if (clientRect.top + clientRect.height > topLimit) {
+        this.elementsList.style.top = clientRect.top + clientRect.height + 'px';
+      } else {
+        this.elementsList.style.top = topLimit + 'px';
+      }
+    }
+    if (adjustLeft) {
+      if (clientRect.left + this.elementsList.offsetWidth < window.innerWidth) {
+        this.elementsList.style.left = clientRect.left + 'px';
+      } else {
+        this.elementsList.style.left = '0px';
+      }
+    }
+  }
 }
