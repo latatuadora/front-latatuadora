ba7e10bbf2ad83ad18d436379327e73a879407a3
enrique199413@gmail.com <enrique199413@gmail.com>
2017-06-06 13:47:24 -0500

    #9 adapt the resources

--- src/utils/web-api.js
+++ src/utils/web-api.js
@@ -3,7 +3,8 @@ import {HttpClient} from 'aurelia-http-client';
 let client = new HttpClient();
 
 client.configure(x => {
-  x.withBaseUrl('http://70.35.203.91');
+  x.withBaseUrl('http://70.35.203.91/');
+  x.withHeader('Accept', 'application/json');
 });
 
 export class WebAPI {
