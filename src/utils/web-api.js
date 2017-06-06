77bd15a8ae8168559a525c54f84139de704f3022
Alan Sanchez <sanchezpineda03@gmail.com>
2017-06-05 23:52:36 -0500

    #9. Implemented postQuotationRequest in webAPI.

--- src/utils/web-api.js
+++ src/utils/web-api.js
@@ -1,5 +1,13 @@
-import {HttpClient, json} from 'aurelia-http-client';
+import {HttpClient} from 'aurelia-http-client';
+
+let client = new HttpClient();
+
+client.configure(x => {
+  x.withBaseUrl('http://70.35.203.91');
+});
 
 export class WebAPI {
-  
+  postQuotationRequest(request) {
+    return client.post('quotation', request);
+  }
 }
