706c973f5737d9524429fbeffc8f3dfad144fdd6
Alan Sanchez <sanchezpineda03@gmail.com>
2017-06-15 17:38:06 -0500

    #32. Basic implementation of swiper carousel in studio profile.

--- src/pages/artist/studio/studio.js
+++ src/pages/artist/studio/studio.js
@@ -1,3 +1,32 @@
+import {inject} from 'aurelia-framework';
+import {MockAPI} from 'utils/mock-api';
+
+@inject(MockAPI)
 export class Studio {
-  
+  constructor(api) {
+    this.api = api;
+    this.initCarousel = false;
+    this.carouselOptions = {
+      pagination: '.swiper-pagination',
+      slidesPerView: 4,
+      loop: true,
+      spaceBetween: 30,
+      paginationClickable: true,
+      breakpoints: [
+        1024: {
+          slidesPerView: 1
+        }
+      ]
+    };
+    this.tattoos = [];
+    this.getTattoos();
+  }
+
+  getTattoos() {
+    this.api.getTattoos()
+      .then(tattoos => {
+        this.tattoos = tattoos;
+        this.initCarousel = true;
+      });
+  }
 }
