8dd4684f3f4cb3c51908b1dd2a742719daf52679
Alan Sanchez <sanchezpineda03@gmail.com>
2017-06-15 20:33:52 -0500

    #32. Flash-square-item markup.

--- src/resources/elements/flash-square-item/flash-square-item.js
+++ src/resources/elements/flash-square-item/flash-square-item.js
@@ -1,6 +1,11 @@
+import {bindable, containerless} from 'aurelia-framework';
 import {ArtistItem} from 'resources/elements/artist-item/artist-item';
 
+@containerless
 export class FlashSquareItem extends ArtistItem {
+  @bindable item;
+  @bindable animate = false;
+
   constructor() {
     super();
   }
