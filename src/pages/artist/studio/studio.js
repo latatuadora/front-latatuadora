15fd09f48123214c867cc880a224264043041be4
Alan Sanchez <sanchezpineda03@gmail.com>
2017-06-15 21:12:21 -0500

    #32. Implemented swiper carousel in flashes items.

--- src/pages/artist/studio/studio.js
+++ src/pages/artist/studio/studio.js
@@ -1,13 +1,17 @@
 import {inject} from 'aurelia-framework';
 import {MockAPI} from 'utils/mock-api';
 
-let baseCarousel = {
-  pagination: '.swiper-pagination',
-  prevButton: '.swiper-button-prev',
-  nextButton: '.swiper-button-next',
-  slidesPerView: 'auto',
-  loop: true,
-  paginationClickable: true
+class Carousel {
+  constructor(includeArrows) {
+    this.pagination = '.swiper-pagination';
+    if (includeArrows) {
+      this.prevButton = '.swiper-button-prev';
+      this.nextButton = '.swiper-button-next';
+    }
+    this.slidesPerView = 'auto';
+    this.loop = true;
+    this.paginationClickable = true;
+  }
 };
 
 @inject(MockAPI)
@@ -17,12 +21,15 @@ export class Studio {
     this.initTattoos = false;
     this.initFlashes = false;
     this.initPhotos = false;
-    this.tattoosOptions = baseCarousel;
+    this.tattoosOptions = new Carousel(true);
     this.tattoosOptions.spaceBetween = 5;
-    this.flashesOptions = baseCarousel;
+    this.flashesOptions = new Carousel(false);
+    this.flashesOptions.slidesPerView = 4;
+    this.flashesOptions.slidesPerColumn = 2;
     this.tattoos = [];
     this.flashes = [];
     this.getTattoos();
+    this.getFlashes();
   }
 
   getTattoos() {
