03b63858e981ff4c6895d746bedd72b2cc7fe7c0
Alan Sanchez <sanchezpineda03@gmail.com>
2017-06-15 20:58:20 -0500

    #32. Changes to studio viewmodel.

--- src/pages/artist/studio/studio.js
+++ src/pages/artist/studio/studio.js
@@ -1,21 +1,27 @@
 import {inject} from 'aurelia-framework';
 import {MockAPI} from 'utils/mock-api';
 
+let baseCarousel = {
+  pagination: '.swiper-pagination',
+  prevButton: '.swiper-button-prev',
+  nextButton: '.swiper-button-next',
+  slidesPerView: 'auto',
+  loop: true,
+  paginationClickable: true
+};
+
 @inject(MockAPI)
 export class Studio {
   constructor(api) {
     this.api = api;
-    this.initCarousel = false;
-    this.carouselOptions = {
-      pagination: '.swiper-pagination',
-      prevButton: '.swiper-button-prev',
-      nextButton: '.swiper-button-next',
-      slidesPerView: 'auto',
-      loop: true,
-      spaceBetween: 5,
-      paginationClickable: true
-    };
+    this.initTattoos = false;
+    this.initFlashes = false;
+    this.initPhotos = false;
+    this.tattoosOptions = baseCarousel;
+    this.tattoosOptions.spaceBetween = 5;
+    this.flashesOptions = baseCarousel;
     this.tattoos = [];
+    this.flashes = [];
     this.getTattoos();
   }
 
@@ -23,7 +29,15 @@ export class Studio {
     this.api.getTattoos()
       .then(tattoos => {
         this.tattoos = tattoos;
-        this.initCarousel = true;
+        this.initTattoos = true;
+      });
+  }
+
+  getFlashes() {
+    this.api.getTattoos()
+      .then(flashes => {
+        this.flashes = flashes;
+        this.initFlashes = true;
       });
   }
 }
