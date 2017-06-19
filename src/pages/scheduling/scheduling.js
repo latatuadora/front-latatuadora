82ef3ffb5cf1b4a087893d032497f304cb408f39
Alan Sanchez <sanchezpineda03@gmail.com>
2017-06-18 22:43:04 -0500

    #38. Added class dependencies to scheduling viewmodel.

--- src/pages/scheduling/scheduling.js
+++ src/pages/scheduling/scheduling.js
@@ -1,6 +1,22 @@
-export class Scheduling {
-  constructor() {
+import {inject, NewInstance} from 'aurelia-framework';
+import {Validator, ValidationRules, ValidationController, validateTrigger} from 'aurelia-validation';
+import {MockAPI} from 'utils/mock-api';
 
+@inject(MockAPI, NewInstance.of(ValidationController), Validator)
+export class Scheduling {
+  constructor(api, controller, validator) {
+    this.api = api;
+    this.controller = controller;
+    this.validator = validator;
+
+    this.artist = null;
+
+    this.controller.validateTrigger = validateTrigger.changeOrBlur;
+    this.maxFileSize = 5 * 1024 * 1024;
+    this.fileErrors = {
+      type: false,
+      size: false
+    };
   }
 
   activate(params, routeConfig) {
@@ -8,5 +24,9 @@ export class Scheduling {
   }
 
   getArtist(id) {
+    this.api.getArtist(id)
+      .then(artist => {
+        this.artist = artist;
+      });
   }
 }
