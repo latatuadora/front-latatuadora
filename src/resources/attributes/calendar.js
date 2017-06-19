41db280897cba491630bf241cbc1e45e3da1b40b
Alan Sanchez <sanchezpineda03@gmail.com>
2017-06-18 23:10:23 -0500

    #38. Flatpickr attribute changes.

--- src/resources/attributes/calendar.js
+++ src/resources/attributes/calendar.js
@@ -1,7 +1,27 @@
-import {bindable} from 'aurelia-framework';
+import {bindable, inject} from 'aurelia-framework';
 import flatpickr from 'flatpickr';
 
+@inject(Element)
 export class CalendarCustomAttribute {
-  @bindable options;
+  @bindable options = {inline: true};
   @bindable value;
+
+  constructor(element) {
+    this.element = element;
+    this.options.onChange = this.onChange;
+  }
+
+  bind() {
+    if (!this.value || !(this.value instanceof Date)) {
+      throw new Error('Value not found or not a Date object');
+    }
+  }
+
+  attached() {
+    this.calendar = flatpickr(this.element, this.options);
+  }
+
+  onChange = (value) => {
+    this.value = value;
+  }
 }
