61ea47b5976572f82b9f98b1c265bb8192d23b84
Alan Sanchez <sanchezpineda03@gmail.com>
2017-06-22 18:11:07 -0500

    #44. Added profile picture logic and styles.

--- src/dashboards/user/edit/edit.js
+++ src/dashboards/user/edit/edit.js
@@ -1,3 +1,29 @@
 export class Edit {
+  constructor() {
+    this.maxFileSize = 5 * 1024 * 1024;
+    this.fileErrors = {
+      type: false,
+      size: false
+    };
+    this.fields = {
+      file: null
+    };
+  }
 
+  onLoaded = (file, data) => {
+    this.fields.file = {
+      file: file,
+      data: data
+    };
+    this.fileErrors.type = false;
+    this.fileErrors.size = false;
+  }
+
+  onError = (file, error) => {
+    if (error == 'File type does not match filter') {
+      this.fileErrors.type = true;
+    } else {
+      this.fileErrors.size = true;
+    }
+  }
 }
