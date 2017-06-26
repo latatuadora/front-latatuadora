fc95053410c4e4f79c118e1af2aabe7514bafb24
Alan Sanchez <sanchezpineda03@gmail.com>
2017-06-25 22:53:40 -0500

    #45. Favourites extends from basegallery.

--- src/dashboards/user/favourites/favourites.js
+++ src/dashboards/user/favourites/favourites.js
@@ -1,3 +1,11 @@
-export class Favourites {
-  
+import {inject} from 'aurelia-framework';
+import {BaseGallery} from 'utils/base-gallery';
+import {MockAPI} from 'utils/mock-api';
+
+@inject(MockAPI)
+export class Favourites extends BaseGallery {
+  constructor(api) {
+    super(api);
+    this.apiMethod = 'getFavourites';
+  }
 }
