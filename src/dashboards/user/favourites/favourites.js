614422ddd59ebdbfdd4025bcdb5b6645bb9aaa6b
Alan Sanchez <sanchezpineda03@gmail.com>
2017-06-25 23:05:17 -0500

    #45. Added logic to favourites.

--- src/dashboards/user/favourites/favourites.js
+++ src/dashboards/user/favourites/favourites.js
@@ -7,5 +7,48 @@ export class Favourites extends BaseGallery {
   constructor(api) {
     super(api);
     this.apiMethod = 'getFavourites';
+    this.params = {
+      type1: '',
+      type2: '',
+      page: 1
+    };
+    this.lists = {
+      type1: [],
+      type2: []
+    };
+    this.activeIds = {
+      type1: -1,
+      type2: -1
+    };
+    this.showDropdowns = {
+      type1: false,
+      type2: false
+    };
+  }
+
+  getLists() {
+    this.getType1();
+    this.getType2();
+  }
+
+  resetParams() {
+    this.params = {
+      type1: '',
+      type2: '',
+      page: 1
+    };
+    this.activeIds = {
+      type1: -1,
+      type2: -1
+    };
+    this.allEmpty = true;
+  }
+
+  getType1() {
+    this.lists.type1 = ['Tatuajes', 'Flashes'];
+  }
+
+  getType2() {
+    this.lists.type2 = ['Estudio', 'Freelancer'];
   }
 }
