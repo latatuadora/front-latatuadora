57dc2668e03098b423e6faedbe7266144436262d
Alan Sanchez <sanchezpineda03@gmail.com>
2017-06-27 18:47:18 -0500

    #46. Added sessionRoutes and rolesStrategy to app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-export class App {
-  configureRouter (config, router) {
-    let postRender = new PostRenderStep();
-    this.router = router;
+import {inject} from 'aurelia-framework';
+import {Session} from 'utils/session';
 
-    config.addPostRenderStep(postRender);
-    config.title = 'La Tatuadora';
-    config.map([
+@inject(Session)
+export class App {
+  constructor(session) {
+    this.session = session;
+    this.baseRoutes = [
       {
         route: ['', 'home'],
         name: 'home',
@@ -94,13 +94,84 @@ export class App {
         nav: false
       },
       {
-        route: 'dashboard',
-        name: 'dashboard',
-        moduleId: 'dashboards/index',
-        title: 'Dashboard',
+        route: 'error',
+        name: 'error',
+        moduleId: 'pages/homepage/homepage',
+        title: 'Error',
         nav: false
       }
-    ]);
+    ];
+    this.sessionRoutes = [
+      {
+        route: 'dashboard',
+        redirects: {
+          1: 'perfil/editar'
+        },
+        nav: false,
+        role: 1,
+        navigationStrategy: this.rolesStrategy
+      },
+      {
+        route: 'cotizaciones',
+        name: 'quotations',
+        moduleId: 'dashboards/user/quotations/quotations',
+        title: 'Cotizaciones',
+        nav: true,
+        level: 1,
+        role: 1,
+        navigationStrategy: this.rolesStrategy
+      },
+      {
+        route: 'favoritos',
+        name: 'favourites',
+        moduleId: 'dashboards/user/favourites/favourites',
+        title: 'Me gusta',
+        nav: true,
+        level: 1,
+        role: 1,
+        navigationStrategy: this.rolesStrategy
+      },
+      {
+        route: 'perfil/editar',
+        name: 'edit_profile',
+        moduleId: 'dashboards/user/edit/edit',
+        title: 'Editar perfil',
+        nav: true,
+        level: 1,
+        role: 1,
+        navigationStrategy: this.rolesStrategy
+      }
+    ];
+  }
+
+  configureRouter (config, router) {
+    let postRender = new PostRenderStep();
+    this.router = router;
+
+    config.addPostRenderStep(postRender);
+    config.title = 'La Tatuadora';
+    config.map(this.baseRoutes.concat(this.sessionRoutes));
+  }
+
+  rolesStrategy = (instruction) => {
+    let type = this.session.userType.toString();
+    let isAllowed = this.session.typeMatches(instruction.config.role);
+
+    if (isAllowed) {
+      if (instruction.config.redirects) {
+        if (instruction.config.redirects[type] == undefined) {
+          throw new Error('There is no redirection defined for this role.');
+        }
+        instruction.config.redirect = instruction.config.redirects[type];
+      } else if (instruction.config.modules) {
+        if (instruction.config.modules[type] == undefined) {
+          throw new Error('There is no module defined for this role.');
+        }
+        instruction.config.moduleId = instruction.config.modules[type];
+      }
+    } else {
+      instruction.config.redirect = 'error';
+    }
   }
 }
 
