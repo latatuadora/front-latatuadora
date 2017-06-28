f46d5a77d389f6914e1e67ba00d44bf3e427b562
Alan Sanchez <sanchezpineda03@gmail.com>
2017-06-27 20:22:40 -0500

    #46. Added login validation.

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,17 +1,42 @@
-import {inject} from 'aurelia-framework';
+import {inject, NewInstance} from 'aurelia-framework';
+import {Validator, ValidationRules, ValidationController, validateTrigger} from 'aurelia-validation';
 import {Session} from 'utils/session';
 
-@inject(Session)
+@inject(NewInstance.of(ValidationController), Validator, Session)
 export class Login {
-  constructor(session) {
+  constructor(controller, validator, session) {
+    this.controller = controller;
+    this.controller.validateTrigger = validateTrigger.changeOrBlur;
+    this.validator = validator;
     this.session = session;
     this.fields = {
       email: '',
       password: ''
     };
+    this.setRules();
+  }
+
+  setRules() {
+    ValidationRules
+      .ensure('email')
+      .required()
+      .withMessage('Introduce tu dirección de correo electrónico')
+      .email()
+      .withMessage('Debes introducir una dirección de correo válida')
+      .ensure('password')
+      .required()
+      .withMessage('Introduce tu contraseña')
+      .minLength(6)
+      .withMessage('La contraseña debe de tener al menos 6 caracteres')
+      .on(this.fields);
   }
 
   login() {
-    this.session.authService.login(this.fields);
+    this.controller.validate()
+      .then(validation => {
+        if (validation.valid) {
+          this.session.authService.login(this.fields);
+        }
+      });
   }
 }
