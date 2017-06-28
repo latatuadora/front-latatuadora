62227a576c967993035c93df6e98e54ffe45ad71
Alan Sanchez <sanchezpineda03@gmail.com>
2017-06-28 16:36:15 -0500

    #42. Implemented getTattoos and getFlashes web-api methods.

--- src/utils/web-api.js
+++ src/utils/web-api.js
@@ -3,6 +3,16 @@ import {MockAPI} from 'utils/mock-api';
 
 let client = new HttpClient();
 
+function getQuery(params) {
+  let queryArray = [];
+  for (let key in params) {
+    if (params[key] || params[key] === false) {
+      queryArray.push(key + '=' + params[key]);
+    }
+  }
+  return queryArray.length ? '?' + queryArray.join('&') : '';
+}
+
 client.configure(x => {
   x.withBaseUrl('http://35.161.232.194:1337/')
     .withDefaults({
@@ -18,6 +28,26 @@ export class WebAPI extends MockAPI {
     return client.fetch('quotation', {
       method: 'post',
       body: json(request)
+    })
+    .then(response => {
+      return response.json();
+    });
+  }
+
+  getTattoos(params) {
+    let query = getQuery(params);
+    return client.fetch('tattoo' + query, {
+      method: 'get'
+    })
+    .then(response => {
+      return response.json();
+    });
+  }
+
+  getFlashes(params) {
+    let query = getQuery(params);
+    return client.fetch('flash' + query, {
+      method: 'get'
     })
     .then(response => {
       return response.json();
