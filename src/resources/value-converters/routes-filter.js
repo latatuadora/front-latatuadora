7375619d839c6d646267e2c5835f68326a04eec7
Alan Sanchez <sanchezpineda03@gmail.com>
2017-06-27 21:03:25 -0500

    #46. Hidden login link when already logged.

--- src/resources/value-converters/routes-filter.js
+++ src/resources/value-converters/routes-filter.js
@@ -1,11 +1,20 @@
+import {inject} from 'aurelia-framework';
+import {Session} from 'utils/session';
+
+@inject(Session)
 export class RoutesFilterValueConverter {
+  constructor(session) {
+    this.session = session;
+  }
+
   toView(routes, level = 0, role = 0) {
     return routes.filter(route => {
       let navRoles = route.config.roles ? route.config.roles : [0];
       let navLevel = route.config.level ? route.config.level : 0;
       let roleMatches = navRoles.indexOf(role) != -1;
+      let hideLogin = route.config.name == 'login' && this.session.authService.isAuthenticated();
 
-      return roleMatches && level == navLevel;
+      return roleMatches && level == navLevel && !hideLogin;
     });
   }
 }
