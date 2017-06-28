c12edb2c180fe20a260dac4265bc13aa0823849e
Alan Sanchez <sanchezpineda03@gmail.com>
2017-06-27 20:11:01 -0500

    #46. Modified filter-routes.

--- src/resources/value-converters/filter-routes.js
+++ src/resources/value-converters/filter-routes.js
@@ -1,16 +1,10 @@
 export class FilterRoutesValueConverter {
   toView(routes, level = 0, role = 0) {
     return routes.filter(route => {
-      let navRole = route.config.role ? route.config.role : 0;
+      let navRoles = route.config.roles ? route.config.roles : [0];
       let navLevel = route.config.level ? route.config.level : 0;
-      let roleMatches;
+      let roleMatches = navRoles.indexOf(role) != -1;
 
-      if (Array.isArray(navRole)) {
-        roleMatches = navRole.indexOf(role) != -1;
-      } else {
-        roleMatches = navRole == role;
-      }
-      
       return roleMatches && level == navLevel;
     });
   }
