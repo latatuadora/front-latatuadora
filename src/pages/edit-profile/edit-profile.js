d2e75e7329326228367830e3423938fc08c709d6
Raúl Marty <santor3medio@gmail.com>
2017-07-06 18:01:34 -0500

    50. Include description and address text.

--- src/pages/edit-profile/edit-profile.js
+++ src/pages/edit-profile/edit-profile.js
@@ -1,6 +1,8 @@
 export class EditProfile {
   constructor () {
     this.profile = ["Mi", "Perfil"];
+    this.description = "Llevamos la cultura del tatuaje a otro nivel. En Dalí Ink buscamos a los mejores artistas del tatuaje de todo el mundo y los traemos a México para ti. Mantente atento a nuestras fechas, agenda tu cita y lleva sobre tu piel una obra de arte.";
+    this.address = "Avenida Alvaro Obregón 32, Col. Roma Norte, delegación Cuauhtemoc, C.P. 06700, CDMX.";
     this.tags = [
       {
         name: 'Old School'
