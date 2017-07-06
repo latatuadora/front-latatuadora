f6f81bffa6bdebd9cd6a75df546b8c445afd30dc
Raúl Marty <santor3medio@gmail.com>
2017-07-06 10:55:07 -0500

    Add js file to edit-profile. Create objects of tags of Mis estilos section

---
+++ src/pages/edit-profile/edit-profile.js
@@ -0,0 +1,52 @@
+export class EditProfile {
+  constructor () {
+    this.profile = ["Mi", "Perfil"];
+    this.tags = [
+      {
+        name: 'Old School'
+      },
+      {
+        name: 'Machine'
+      },
+      {
+        name: 'Geométrico'
+      },
+      {
+        name: 'Skull'
+      },
+      {
+        name: 'Skull'
+      },
+      {
+        name: 'Old School'
+      },
+      {
+        name: 'Machine'
+      },
+      {
+        name: 'Geométrico'
+      },
+      {
+        name: 'Skull'
+      },
+      {
+        name: 'Skull'
+      },
+      {
+        name: 'Old School'
+      },
+      {
+        name: 'Machine'
+      },
+      {
+        name: 'Geométrico'
+      },
+      {
+        name: 'Skull'
+      },
+      {
+        name: 'Skull'
+      }
+    ];
+  }
+}
