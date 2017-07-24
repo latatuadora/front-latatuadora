252ea348ca0e9c9ca6fa9c95ded72f0b6606c5fd
Raul Marty <santor3medio@gmail.com>
2017-07-24 11:07:40 -0500

    #71. Add js file to favorite-studies.

---
+++ src/pages/favorite-studies/favorite-studies.js
@@ -0,0 +1,10 @@
+export class FavoriteStudies {
+  constructor () {
+		this.favoriteStudies = {
+			title: "Estudios favoritos",
+			subtitleFirts: "Aquí encontrarás",
+			subtitleSecond: "los estudios y tatuadores",
+			subtitleThird: "que haz marcado como favoritos"
+		}
+  }
+}
