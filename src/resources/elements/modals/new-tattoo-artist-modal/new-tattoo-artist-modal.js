dca98b377332b51a84e8caff5730288c640b060b
Raul Marty <santor3medio@gmail.com>
2017-07-27 12:50:09 -0500

    #77. Import bindables in js file new-tattoo-artist-modal.

---
+++ src/resources/elements/modals/new-tattoo-artist-modal/new-tattoo-artist-modal.js
@@ -0,0 +1,11 @@
+import {bindable} from 'aurelia-framework';
+
+export class NewTattooArtistModal {
+  @bindable flash;
+  @bindable type = 'tattoo';
+  @bindable vote;
+  @bindable comment;
+  @bindable goPrev;
+  @bindable goNext;
+  @bindable close;
+}
