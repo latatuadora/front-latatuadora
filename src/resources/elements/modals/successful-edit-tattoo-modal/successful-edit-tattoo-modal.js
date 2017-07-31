2bf9f2d048b0ac15012662cf84f329b5cfb21e9a
Raul Marty <santor3medio@gmail.com>
2017-07-31 13:40:05 -0500

    #85. Add bindables in js file of successful-edit-tattoo-modal.

---
+++ src/resources/elements/modals/successful-edit-tattoo-modal/successful-edit-tattoo-modal.js
@@ -0,0 +1,11 @@
+import {bindable} from 'aurelia-framework';
+
+export class SuccessfulEditTattooModal {
+  @bindable flash;
+  @bindable type = 'tattoo';
+  @bindable vote;
+  @bindable comment;
+  @bindable goPrev;
+  @bindable goNext;
+  @bindable close;
+}
