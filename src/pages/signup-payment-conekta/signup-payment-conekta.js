2efb2e9df6adc30d15a18df6bcb3b396bd464ccf
Raul Marty <santor3medio@gmail.com>
2017-07-31 13:34:02 -0500

    #85. Import BaseModal in js file signup-payment-conekta.

--- src/pages/signup-payment-conekta/signup-payment-conekta.js
+++ src/pages/signup-payment-conekta/signup-payment-conekta.js
@@ -1,5 +1,8 @@
-export class SignupPaymentConekta {
+import {BaseModal} from 'utils/base-modal';
+
+export class SignupPaymentConekta extends BaseModal{
   constructor () {
+    super();
     this.paySignup = {
       title: "Pago",
       signupPrice: "1000.00"
