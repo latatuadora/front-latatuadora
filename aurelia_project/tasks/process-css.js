039b7dca4f0202e7cf0a9bfc530e047641b9d3ab
Alessandro Aguilar <danielalessandro.aguilar.contractor@bbva.com>
2017-08-28 19:07:20 -0500

    Change Task, Build css dont break on error

--- aurelia_project/tasks/process-css.js
+++ aurelia_project/tasks/process-css.js
@@ -10,6 +10,9 @@ export default function processCSS() {
   return gulp.src(project.cssProcessor.source)
     .pipe(changedInPlace({firstPass: true}))
     .pipe(sourcemaps.init())
-    .pipe(stylus({use: [autoprefixerStylus('last 2 versions')]}))
+    .pipe(stylus({use: [autoprefixerStylus('last 2 versions')]})).on('error', function(error) {
+      console.log(error.message)
+      this.emit('END')
+    })
     .pipe(build.bundle());
 }
