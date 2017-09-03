a861531416c5cc357f6c95af84e94933f792648e
Alessandro Aguilar <danielalessandro.aguilar.contractor@bbva.com>
2017-09-03 14:57:27 -0500

    add controller for artist controller

---
+++ src/controller/artist.js
@@ -0,0 +1,29 @@
+import { Client } from './client'
+import {inject, computedFrom} from 'aurelia-framework';
+
+export class Artist extends Client {
+
+  constructor() {
+    super()
+    this.endpoint = 'artist'
+  }
+
+  artist(id) {
+
+    const url = this.URL(this.endpoint, `${id}`)
+
+    this.client
+      .fetch(`${url}`, {
+        method: this.methods.get
+      })
+      .then(response => response.json())
+      .then(artist => {
+        return artist
+      })
+      .catch(error => {
+        return error
+      })
+
+  }
+
+}
