94a52086a868cf5cfe4f2b1a7762b00a600de3a8
Alessandro Aguilar <danielalessandro.aguilar.contractor@bbva.com>
2017-09-03 14:57:05 -0500

    Add controller for quotation endpoint

---
+++ src/controller/quotation.js
@@ -0,0 +1,29 @@
+import { Client } from './client'
+import {AuthService} from 'aurelia-authentication';
+import {inject, computedFrom} from 'aurelia-framework';
+import {json} from 'aurelia-fetch-client';
+
+export class Quotation extends Client {
+
+  constructor() {
+    super()
+    console.log(this.client)
+  }
+
+  quotation(body) {
+    const endpoint = 'quotation'
+    return this.client
+      .fetch(`${endpoint}`,{
+        method: this.methods.post,
+        body: json(body)
+      })
+      .then(response => response.json())
+      .then(quotation => {
+        return quotation
+      })
+      .catch(error => {
+        return error
+      })
+  }
+
+}
