e6e7c05c4839372103276dc5e10bc4e467269676
Alessandro Aguilar <danielalessandro.aguilar.contractor@bbva.com>
2017-09-03 14:58:36 -0500

    Change configuration to new based on framework documentation.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,22 @@
 import fetch from 'whatwg-fetch';
 import environment from './environment';
 
-let authConfig = {
-  endpoint: 'login',
-  loginUrl: 'login',
-  loginRedirect: '#/dashboard',
-  loginRoute: '#/login'
-};
-
 export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
     .feature('resources')
     .plugin('aurelia-validation')
     .plugin('aurelia-api', config => {
-      config.registerEndpoint('login', 'http://35.161.232.194:1337/')
-        .setDefaultEndpoint('login');
+      config.registerEndpoint('api', 'http://sandbox.latatuadora.getmore.mx:1337/')
+        .setDefaultEndpoint('api');
     })
     .plugin('aurelia-authentication', baseConfig => {
-      baseConfig.configure(authConfig);
+      baseConfig.configure({
+        endpoint: 'api',
+        loginUrl: 'login',
+        loginRedirect: '#/dashboard',
+        loginRoute: '#/login'
+      });
     })
     .plugin('aurelia-files');
 
