f2fe9e0c159e155237694f78ef0a057df05a555b
Alessandro Aguilar <danielalessandro.aguilar.contractor@bbva.com>
2017-09-12 08:49:35 -0500

    Change visually signup

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -1,20 +1,22 @@
-import {BaseModal} from 'utils/base-modal';
 
-export default class Signup extends BaseModal {
-  constructor () {
-    super();
+
+export default class Signup {
+
+  constructor() {
     this.tags = [
-      'OldSchool',
-      'PostModern',
-      'Modern',
-      'Geometric',
-      'Classic',
-      'Pattern'
-    ]
+      {id: 1,  name: 'Old School', imgUrl:'/src/assets/images/mock/style14.jpg' },
+      {id: 2,  name: 'New School', imgUrl:'/src/assets/images/mock/style15.jpg' },
+      {id: 3,  name: 'Puntillismo', imgUrl:'/src/assets/images/mock/style10.png' },
+      {id: 4,  name: 'Geometrico', imgUrl:'/src/assets/images/mock/style9.png' },
+      {id: 5,  name: 'Trash Polka', imgUrl:'/src/assets/images/mock/style7.png' },
+      {id: 6,  name: 'Black work', imgUrl:'/src/assets/images/mock/style1.png' },
+      {id: 7,  name: 'Acuarela', imgUrl:'/src/assets/images/mock/style13.png' },
+      {id: 8,  name: 'Japones', imgUrl:'/src/assets/images/mock/style13.png' },
+      {id: 9,  name: 'Tribal', imgUrl:'/src/assets/images/mock/style16.jpg' },
+      {id: 10, name: 'Caligrafia', imgUrl:'/src/assets/images/mock/style11.png' },
+      {id: 11, name: 'Ilustracion', imgUrl:'/src/assets/images/mock/style12.png' },
+      {id: 12, name: 'Surreal', imgUrl:'/src/assets/images/mock/style6.png' },
+      {id: 13, name: 'Biomecanico', imgUrl:'/src/assets/images/mock/style8.png' }
+    ];
   }
-
-
-
-
-
 }
