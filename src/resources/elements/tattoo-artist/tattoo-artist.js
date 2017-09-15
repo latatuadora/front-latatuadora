55d14b4c3521581c8a3eff050830d231952572fb
Alessandro Aguilar <danielalessandro.aguilar.contractor@bbva.com>
2017-09-15 16:08:17 -0500

    CHANGE | {tattoo-artist}  - ?

--- src/resources/elements/tattoo-artist/tattoo-artist.js
+++ src/resources/elements/tattoo-artist/tattoo-artist.js
@@ -1,27 +1,38 @@
-import { bindable } from 'aurelia-framework';
+import { bindable, inject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
 
+@inject(Router)
 export class TattooArtist {
 
-  @bindable artist;
+  @bindable artist
 
-  bind() {
-    this.artist.styles && this.__styles()
+  constructor (router) {
+    this.router = router;
   }
 
-  __styles() {
+  bind() {
+    this.artist.styles && this.styles()
+  }
+
+  styles() {
 
     const medium = matchMedia('(min-width: 512px)')
     const checkStyles = mediaquerie => {
 
       this.styles = mediaquerie.matches
-                  ? this.artist.styles.slice(0, 4)
-                  : this.artist.styles.slice(0, 2)
+        ? this.artist.styles.slice(0, 4)
+        : this.artist.styles.slice(0, 2)
 
     }
 
     medium.addListener(checkStyles)
     checkStyles(medium)
 
+  }
+
+  studio () {
+    if(this.artist.id) this.router.navigateToRoute('studio',{ id: this.artist.id })
+    else this.router.navigateToRoute('inspirate')
   }
 
 
