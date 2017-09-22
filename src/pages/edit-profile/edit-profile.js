6067896c05b86fe404f2c656c44dbaa379b138a3
enrique199413@gmail.com <enrique199413@gmail.com>
2017-09-21 20:10:55 -0500

    #91 inject session for get the current user

--- src/pages/edit-profile/edit-profile.js
+++ src/pages/edit-profile/edit-profile.js
@@ -1,12 +1,16 @@
 import {BaseModal} from 'utils/base-modal';
+import {Session} from 'utils/session';
+import {inject} from 'aurelia-framework';
 
+@inject(Session)
 export class EditProfile extends BaseModal {
-  constructor () {
+  constructor(session) {
     super();
+    this.session = session;
     this.editProfile = {
       titleWhite: "Mi",
       titleGold: "Perfil"
-    }
+    };
     this.description = "Llevamos la cultura del tatuaje a otro nivel. En Dalí Ink buscamos a los mejores artistas del tatuaje de todo el mundo y los traemos a México para ti. Mantente atento a nuestras fechas, agenda tu cita y lleva sobre tu piel una obra de arte.";
     this.address = "Avenida Alvaro Obregón 32, Col. Roma Norte, delegación Cuauhtemoc, C.P. 06700, CDMX.";
     this.tags = [
@@ -56,5 +60,9 @@ export class EditProfile extends BaseModal {
         name: 'Skull'
       }
     ];
+  }
+  
+  attached() {
+    this.currentUser = this.session.getCurrentUser();
   }
 }
