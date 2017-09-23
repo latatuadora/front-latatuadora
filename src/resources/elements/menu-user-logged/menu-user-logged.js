06773ebec94c7c5bd8e61c04b8db4c18cc01a0b0
enrique199413@gmail.com <enrique199413@gmail.com>
2017-09-22 19:50:55 -0500

    #93 add menu-user-logged js

---
+++ src/resources/elements/menu-user-logged/menu-user-logged.js
@@ -0,0 +1,62 @@
+import {inject} from 'aurelia-framework';
+import {bindable} from 'aurelia-framework';
+import {Session} from 'utils/session';
+
+@inject(Session)
+export class MenuUserLogged {
+  @bindable baseElement;
+  
+  constructor(session) {
+    this.session = session;
+    this.menu = [
+      {
+        text: "Cotizaciones",
+        routePage: 'preguntas/cotizaciones',
+        icon: '/src/assets/images/icons/avatar.svg'
+      },
+      {
+        text: "Me gusta",
+        routePage: 'preguntas/cotizaciones',
+        icon: '/src/assets/images/icons/avatar.svg'
+      },
+      {
+        text: "Editar perfil",
+        routePage: 'dashboard/perfil/editar',
+        icon: '/src/assets/images/icons/avatar.svg'
+      },
+      {
+        text: "Cerrar sesión",
+        routePage: false,
+        icon: '/src/assets/images/icons/avatar.svg'
+      }
+    ];
+    this.handleClick = e => {
+      this.currentElement.classList.toggle('display-inline');
+    };
+  }
+  
+  closeSession() {
+    this.session.logout();
+  }
+  
+  attached() {
+    const size = 20;
+    if (!this.baseElement) {
+      throw new Error('There is no base Element defined for this menu-user-logged');
+    } else {
+      let that = this;
+      let baseElement = document.querySelector('#' + this.baseElement);
+      let curentElement = document.querySelector('menu-user-logged');
+      this.currentElement = curentElement;
+      let positionInViewportBaseElement = baseElement.getBoundingClientRect();
+      curentElement.style.top = positionInViewportBaseElement.top + positionInViewportBaseElement.height + size + 'px';
+      baseElement.addEventListener('click', this.handleClick);
+      console.log(window.getComputedStyle(baseElement, null).width);
+      window.addEventListener('click', function (e) {
+        if (that.currentElement.classList.contains('display-inline') && !e.target.dataset.hasOwnProperty('preventclose')) {
+          that.currentElement.classList.remove('display-inline');
+        }
+      });
+    }
+  }
+}
