2df22380a04693edd0dd5b8cb052c5ff2051380b
enrique199413@gmail.com <enrique199413@gmail.com>
2017-09-26 13:45:48 -0500

    #97 solve if sentence when user not exists

--- src/resources/elements/navbar/navbar.js
+++ src/resources/elements/navbar/navbar.js
@@ -12,7 +12,9 @@ export class Navbar {
   attached() {
     let menu = new Menu(975);
     let currentUser = this.session.getCurrentUser();
-    currentUser.avatar = '/src/assets/images/icons/avatar.svg';
-    this.currentUser = currentUser;
+    if (currentUser) {
+      currentUser.avatar = '/src/assets/images/icons/avatar.svg';
+      this.currentUser = currentUser;
+    }
   }
 }
