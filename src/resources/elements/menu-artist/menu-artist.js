87b9126484ed587c9a00f9b4f43784b309858c96
enrique199413@gmail.com <enrique199413@gmail.com>
2017-09-26 12:35:07 -0500

    #97 comment routes for sprint 1

--- src/resources/elements/menu-artist/menu-artist.js
+++ src/resources/elements/menu-artist/menu-artist.js
@@ -26,41 +26,41 @@ export class MenuArtist {
         route: '/cotizaciones',
         routePage: 'miscotizaciones'
       },
-      {
-        img: 'src/assets/images/icons/np-price.png',
-        text: "Posibles clientes",
-        title: ["Posibles", "clientes"],
-        route: '/clientes',
-        routePage: 'posiblesclientes'
-      },
-      {
-        img: 'src/assets/images/icons/np-clock.png',
-        text: "Solicitudes de agendación",
-        title: ["solicitudes", "de agendación"],
-        route: '../../../pages/menu-page-artist/scheduling-request/scheduling-request',
-        routePage: 'agendacion/solicitud'
-      },
-      {
-        img: 'src/assets/images/icons/avatar.png',
-        text: "Mis tatuadores",
-        title: ["Mis", "tatuadores"],
-        route: '../../../menu-page-artist/my-tattoo-artist/my-tattoo-artist',
-        routePage: 'mistatuadores'
-      },
-      {
-        img: 'src/assets/images/icons/np-price.png',
-        text: "Promocionarme",
-        title: ["Promocionarme"],
-        route: '../../../menu-page-artist/promote-me/promote-me',
-        routePage: 'promocionarme'
-      },
-      {
-        img: 'src/assets/images/icons/np-star.png',
-        text: "Evaluaciones",
-        title: ["evaluaciones"],
-        route: '/evaluaciones',
-        routePage: 'evaluaciones'
-      }
+      // {
+      //   img: 'src/assets/images/icons/np-price.png',
+      //   text: "Posibles clientes",
+      //   title: ["Posibles", "clientes"],
+      //   route: '/clientes',
+      //   routePage: 'posiblesclientes'
+      // },
+      // {
+      //   img: 'src/assets/images/icons/np-clock.png',
+      //   text: "Solicitudes de agendación",
+      //   title: ["solicitudes", "de agendación"],
+      //   route: '../../../pages/menu-page-artist/scheduling-request/scheduling-request',
+      //   routePage: 'agendacion/solicitud'
+      // },
+      // {
+      //   img: 'src/assets/images/icons/avatar.png',
+      //   text: "Mis tatuadores",
+      //   title: ["Mis", "tatuadores"],
+      //   route: '../../../menu-page-artist/my-tattoo-artist/my-tattoo-artist',
+      //   routePage: 'mistatuadores'
+      // },
+      // {
+      //   img: 'src/assets/images/icons/np-price.png',
+      //   text: "Promocionarme",
+      //   title: ["Promocionarme"],
+      //   route: '../../../menu-page-artist/promote-me/promote-me',
+      //   routePage: 'promocionarme'
+      // },
+      // {
+      //   img: 'src/assets/images/icons/np-star.png',
+      //   text: "Evaluaciones",
+      //   title: ["evaluaciones"],
+      //   route: '/evaluaciones',
+      //   routePage: 'evaluaciones'
+      // }
     ];
   }
 }
