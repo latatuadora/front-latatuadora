3de75562dc889bd06c9ddd2a3039cc6cf08659b7
pamlicona <pamela_licona@live.com>
2017-09-29 16:31:52 -0500

    #101 - Recive currentindex

--- src/resources/elements/tattoo-item/tattoo-item.js
+++ src/resources/elements/tattoo-item/tattoo-item.js
@@ -1,7 +1,10 @@
 import {BaseModal} from 'utils/base-modal';
+import {bindable} from 'aurelia-framework';
 
 export class TattooItem extends BaseModal {
-  constructor () {
+  @bindable currentindex;
+  
+  constructor() {
     super();
   }
 }
