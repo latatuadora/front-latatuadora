bdde93619e247375a76ba135020712563a27484c
pamlicona <pamela_licona@live.com>
2017-10-04 17:18:34 -0500

    #105 - Get flashes of studio

--- src/pages/menu-page-artist/my-flashes/my-flashes.js
+++ src/pages/menu-page-artist/my-flashes/my-flashes.js
@@ -1,8 +1,26 @@
 import {BaseModal} from 'utils/base-modal';
+import {Flash} from 'controller/flash';
+import {inject} from 'aurelia-framework';
+import {Session} from 'utils/session';
 
+@inject( Flash, Session)
 export class MyFlashes extends BaseModal{
-  constructor () {
+  constructor(flash, session) {
     super();
+    this.flash = flash;
+    this.session = session;
     this.title = ["Mis", "flashes"];
+    this.dataUser = this.session.getStudioFreelancer();
+  }
+  
+  attached() {
+    let that = this;
+    this.flash.get(this.dataUser.id)
+      .then(response => {
+        that.flashes = response;
+      })
+      .catch(error => {
+        this.error = response;
+      });
   }
 }
