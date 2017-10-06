fbf877bd826cf2cff2996dacd559ab8910074801
pamlicona <pamela_licona@live.com>
2017-10-06 18:51:38 -0500

    #105 - Add artist request

--- src/controller/artist.js
+++ src/controller/artist.js
@@ -1,47 +1,49 @@
-import { Client } from './client'
-import {inject, computedFrom} from 'aurelia-framework';
+import { Client } from './client';
 
 export class Artist extends Client {
 
   constructor() {
     super()
-    this.endpoint = 'artist'
+    this.endpoint = 'artist';
   }
 
   artist(id) {
-
-    const url = this.URL(this.endpoint, `${id}`)
-
+    const url = this.URL(this.endpoint, `${id}`);
     return this.client
       .fetch(`${url}`, {
         method: this.methods.get
       })
       .then(response => response.json())
       .then(artist => {
-        return artist
+        return artist;
       })
       .catch(error => {
-        return error
-      })
-
+        return error;
+      });
   }
 
   find(params) {
-
-    const url = this.URL(this.endpoint, params)
-
+    const url = this.URL(this.endpoint, params);
     return this.client
       .fetch(`${url}`, {
         method: this.methods.get
       })
       .then(response => response.json())
       .then(artist => {
-        return artist
+        return artist;
       })
       .catch(error => {
-        return error
-      })
-
+        return error;
+      });
+  }
+  
+  add(artist) {
+    let that = this;
+    return new Promise (function(accept, reject) {
+      return that.simplePetition('artist', 'POST', artist)
+        .then(data => accept(data))
+        .catch(error => reject(error));
+    });
   }
 
 }
