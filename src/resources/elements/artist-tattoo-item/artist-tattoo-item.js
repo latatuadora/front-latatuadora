1a4c2b040ade73b5ca1ddec93efd37f1ccec6b70
pamlicona <pamela_licona@live.com>
2017-10-10 19:14:00 -0500

    #110 - Bindable currentindex

--- src/resources/elements/artist-tattoo-item/artist-tattoo-item.js
+++ src/resources/elements/artist-tattoo-item/artist-tattoo-item.js
@@ -2,6 +2,7 @@ import {BaseModal} from 'utils/base-modal';
 import {bindable} from 'aurelia-framework';
 
 export class ArtistTattooItem extends BaseModal {
+  @bindable currentindex;
   @bindable artist;
   
   constructor() {
