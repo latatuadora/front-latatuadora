13d69bc2ada9b5926837b0ccc7487302ee38196d
pamlicona <pamela_licona@live.com>
2017-10-18 17:48:15 -0500

    #123 - Add images for tattoos

--- src/resources/elements/tattoo-item/tattoo-item.js
+++ src/resources/elements/tattoo-item/tattoo-item.js
@@ -7,5 +7,24 @@ export class TattooItem extends BaseModal {
   
   constructor() {
     super();
+    let that = this;
+    this.waitFor(function() {
+      that.getInfo();
+    });
+  }
+  
+  getInfo() {
+    this.image = this.tattoo.image.split('/');
+  }
+  
+  waitFor(cb) {
+    let that = this, timer;
+    
+    timer = setInterval(function() {
+      if (that.tattoo !== null) {
+        clearInterval(timer);
+        cb();
+      }
+    });
   }
 }
