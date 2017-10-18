227371e206101b46f89b14b28469e70d48f642da
pamlicona <pamela_licona@live.com>
2017-10-18 17:45:12 -0500

    #123 - Add images

--- src/resources/elements/flash-item/flash-item.js
+++ src/resources/elements/flash-item/flash-item.js
@@ -8,10 +8,29 @@ export class FlashItem extends BaseModal {
   constructor() {
   	super();
   	//TODO replace with the back send complete data
+    let that = this;
     this.flash = {
       width: '12',
       height: '12',
       likes: '13'
     };
+    this.waitFor(function() {
+      that.getInfo();
+    });
+  }
+  
+  getInfo() {
+    this.image = this.flash.realImageUrl.split('/');
+  }
+  
+  waitFor(cb) {
+    let that = this, timer;
+    
+    timer = setInterval(function() {
+      if (that.flash !== null) {
+        clearInterval(timer);
+        cb();
+      }
+    });
   }
 }
