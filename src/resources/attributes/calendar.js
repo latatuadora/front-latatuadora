eb201a4698744f89aab1546e1454cadcaa53a222
pamlicona <pamela_licona@live.com>
2017-10-19 19:45:39 -0500

    #129 - Show schedule for studio

--- src/resources/attributes/calendar.js
+++ src/resources/attributes/calendar.js
@@ -6,6 +6,8 @@ import es from 'flatpickr_es';
 export class CalendarCustomAttribute {
   @bindable options = {inline: true};
   @bindable value;
+  @bindable schedulesuser;
+  @bindable schedules;
 
   constructor(element) {
     this.element = element;
@@ -14,6 +16,7 @@ export class CalendarCustomAttribute {
   }
 
   bind() {
+    let that = this;
     if (!this.value || !(this.value instanceof Date)) {
       throw new Error('Value not found or not a Date object');
     }
@@ -23,8 +26,22 @@ export class CalendarCustomAttribute {
   attached() {
     this.calendar = flatpickr(this.element, this.options);
   }
-
+  
   onChange = (value) => {
+    let that = this;
     this.value = value;
+    this.intervalId = setInterval(function() {
+      if (that.schedulesuser !== null) {
+        that.schedules = [];
+        that.schedulesuser.forEach(function(day) {
+          if (day.dayId === (value[0].getDay() + 1)) {
+            for(let i = parseInt(day.start); i <= parseInt(day.end); i++) {
+              that.schedules.push(i);
+            }
+          }
+        });
+        clearInterval(that.intervalId);
+      }
+    });
   }
 }
