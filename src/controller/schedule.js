6550b1f27c7025261c19bbdb32f35e7f18f71ae6
pamlicona <pamela_licona@live.com>
2017-10-20 11:56:32 -0500

    #129 - Change request

--- src/controller/schedule.js
+++ src/controller/schedule.js
@@ -8,18 +8,15 @@ export class Schedule extends Client {
   }
   
   schedule(body) {
-    const endpoint = 'itinerary/add';
-    return this.client
-      .fetch(`${endpoint}`,{
-        method: this.methods.post,
-        body: json(body)
-      })
-      .then(response => response.json())
-      .then(quotation => {
-        return quotation;
-      })
-      .catch(error => {
-        return error;
+    let that = this;
+    return new Promise (function(accept, reject) {
+      return that.simpleNativePetition('itinerary/add', 'POST', body, function(success, error) {
+        if (success) {
+          accept(success);
+        } else {
+          reject(error);
+        }
       });
+    });
   }
 }
